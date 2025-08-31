0bc5f7b0a3fd4c0bb6fd0c40c9e14542c08df7a5
ShashankBadger <shashank776052@gmail.com>
2025-08-31 12:25:56 +0530

    Covert product into classes

--- amazon-project/scripts/checkout/orderSummary.js
+++ amazon-project/scripts/checkout/orderSummary.js
@@ -44,7 +44,7 @@ export function renderOrderSummary() {
                     ${matchingProduct.name}
                   </div>
                   <div class="product-price">
-                    ${formatCurrnecy(matchingProduct.priceCents)}
+                    ${matchingProduct.getPrice()}
                   </div>
                   <div class="product-quantity">
                     <span>
