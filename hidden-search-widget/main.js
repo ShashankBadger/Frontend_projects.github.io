3cc19833199619652645be26e600685fb8c00922
ShashankBadger <shashank776052@gmail.com>
2025-11-22 16:22:50 +0530

    Hidden saarch widget project is added

--- hidden-search-widget/main.js
+++ hidden-search-widget/main.js
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  search.classList.toggle('active');
+  input.focus();
+});
