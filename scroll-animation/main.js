1b0cd870348543d3df9e4babe922307da6dbf4f0
ShashankBadger <shashank776052@gmail.com>
2025-11-26 13:05:55 +0530

    scroll animatioon project is added and functionly stable for all the browser

--- scroll-animation/main.js
+++ scroll-animation/main.js
@@ -1,1 +1,20 @@
-console.log('Hello World!')
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerHeight = window.innerHeight / 5 * 4;
+  console.log(triggerHeight);
+
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if(boxTop < triggerHeight){
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
+}
