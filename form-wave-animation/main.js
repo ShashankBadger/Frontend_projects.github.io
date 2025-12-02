1eacf7263aae0086caf2bc1307d487290a0c3930
ShashankBadger <shashank776052@gmail.com>
2025-12-02 11:29:31 +0530

    wave motion is added to the project using the trasfrom and transition properties

--- form-wave-animation/main.js
+++ form-wave-animation/main.js
@@ -1,1 +1,8 @@
-console.log('Hello World!.....')
+const lables = document.querySelectorAll('.form-control label');
+
+lables.forEach((lable) => {
+  lable.innerHTML = lable.innerText
+    .split('')
+    .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
+    .join('')
+});
