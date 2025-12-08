2876b241bf6b3ed991c875e9de1d3dad556c8c96
ShashankBadger <shashank776052@gmail.com>
2025-12-08 13:06:37 +0530

    using the fecth and async/await to call the extranal api

--- dad-jokes/main.js
+++ dad-jokes/main.js
@@ -1,1 +1,21 @@
-console.log("Hello Shashank!")
+const jokeEl = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+generate();
+
+jokeBtn.addEventListener('click', generate)
+
+async function generate(){
+  const config = {
+    headers: {
+    'Accept':'application/json' 
+    }
+  }
+  try {
+    const response = await fetch('https://icanhazdadjoke.com', config);
+    const data = await response.json();
+    jokeEl.innerText = data.joke;
+  } catch(err){
+    console.log("Error:", err);
+  }
+}
