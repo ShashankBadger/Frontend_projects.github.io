0afe6ca835c5c4c70f852a8b2e2a5d434dce1119
ShashankBadger <shashank776052@gmail.com>
2025-12-14 12:50:01 +0530

    insted of index approach i changed parentNode

--- faq-collapse/main.js
+++ faq-collapse/main.js
@@ -1,10 +1,9 @@
 const buttons = document.querySelectorAll('.faq-toggle');
-const faqs = document.querySelectorAll('.faq');
 
 buttons.forEach((button, index) => {
 
   button.addEventListener('click', () => { 
-    faqs[index].classList.toggle('active');
+    button.parentNode.classList.toggle('active');
   });  
 
 });
