81e72f856562ecfe1cd16ab1ef6454a3ec404441
ShashankBadger <shashank776052@gmail.com>
2025-12-14 12:48:06 +0530

    bacis java event listner is added to the project

--- faq-collapse/main.js
+++ faq-collapse/main.js
@@ -1,1 +1,10 @@
-console.log('Hello shashank!')
+const buttons = document.querySelectorAll('.faq-toggle');
+const faqs = document.querySelectorAll('.faq');
+
+buttons.forEach((button, index) => {
+
+  button.addEventListener('click', () => { 
+    faqs[index].classList.toggle('active');
+  });  
+
+});
