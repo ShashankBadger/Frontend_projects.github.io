c3bf237624b99e02eb940d454edc18d35d528844
ShashankBadger <shashank776052@gmail.com>
2025-12-22 11:59:47 +0530

    tags are created and seperated based on comma and stored in arrays form

--- random-choice-picker/main.js
+++ random-choice-picker/main.js
@@ -1,1 +1,20 @@
-console.log("Hello Shahsank!")
+const tagsEl = document.querySelector('#tags');
+const textArea = document.querySelector('#textarea');
+
+textArea.focus();
+
+textArea.addEventListener('keyup', (event) => {
+  createTags(event.target.value);
+});
+
+function createTags(input){
+  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+  tagsEl.innerHTML = '';
+
+  tags.forEach(tag => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
