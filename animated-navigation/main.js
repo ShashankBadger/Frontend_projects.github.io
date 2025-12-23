5a7a60faa092b6a2193a6ee7f9733aad1d2ffd99
ShashankBadger <shashank776052@gmail.com>
2025-12-23 13:34:47 +0530

    toggle the active class in nav section make it interactive

--- animated-navigation/main.js
+++ animated-navigation/main.js
@@ -1,1 +1,6 @@
-console.log("Hello Shashank!")
+const toggle = document.querySelector('#toggle');
+const nav = document.getElementById('nav');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+})
